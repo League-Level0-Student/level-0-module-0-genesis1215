772a098bc2c0b363e15d5013182518c58017fc33
root <root@8b2bc97c337c>
2020-12-17 23:27:34 +0000

    quiz game

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,19 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
+		String code = JOptionPane.showInputDialog ("Do you know how to write a code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
+		if (code.equals("yes")) {
+			
+			JOptionPane.showMessageDialog (null, "you will rule the world");
+			
+		} else {JOptionPane.showMessageDialog (null, "you must sing up for classes at The LEAGUE, because you are not the Brain");
 
+		}
+	
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		
 	}
 }
-
+		
